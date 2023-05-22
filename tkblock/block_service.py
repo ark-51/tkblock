432e90fd1567786078188a90f06d09c77bce86c3
ark-51 <a.k.3125.1@gmail.com>
2023-05-22 22:08:32 +0900

    feat: sampleのテキストにスクロールバーを追加

--- tkblock/block_service.py
+++ tkblock/block_service.py
@@ -385,7 +385,12 @@ class BlockService:
         def echo_text(_):
             print(text1.get("1.0", "end - 1c"))
 
-        text1 = BlockService.create_text(self.frame, 30, 40, 15, 20, name="text1")
+        text1 = BlockService.create_text(
+            self.frame, 30, 40, 15, 20, name="text1", wrap="none"
+        )
+        text1.scrollbar = BlockService.scrollbar(
+            self.frame, x_enable=True, y_enable=True
+        )
         text1.insert(tk.END, "hoge\nfuga")
         BlockService.create_button(self.frame, 30, 40, 20, 21, function=echo_text)
 
